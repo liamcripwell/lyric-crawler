3c906084bd8de60f635021609f702842db32af82
Liam Cripwell <ljcripwell@gmail.com>
2018-01-28 17:17:19 +1000

    script is set up to hopefully mine and save all lyrics for top 50 artists

--- htmlparse.py
+++ htmlparse.py
@@ -2,40 +2,55 @@ import requests
 import codecs
 import lyricParsers
 
-def mineSongs(artistName, artistSongList):
+def mineArtists(page):
+    response = requests.get(page)
+
+    parser = lyricParsers.ArtistParser()
+    print("Finding artists...")
+    parser.feed(str(response.content))
+    print("Number of artists found: " + str(len(parser.artists)))
+    
+    return parser.artists
+
+def mineSongs(artistList):
+    for artist in artistList:
+        print("Mining songs for artist: " + artist[1])
+        response = requests.get(artist[0])
+
+        parser = lyricParsers.ArtistSongParser()
+        parser.feed(str(response.content))
+
+        mineLyrics(artist[1], parser.songs)
+
+def mineLyrics(artistName, artistSongList):
     for song in artistSongList:
         print("Mining lyrics for song: " + song[1])
-        try:
-            file = open("lyrics-data/" + artistName + "/" + song[1], "w") 
-        except "FileNotFoundError":
-            break
-        
         response = requests.get(song[0])
 
-        songParser = SongLyricParser()
-        songParser.feed(str(response.content))
+        parser = lyricParsers.SongLyricParser()
+        parser.feed(str(response.content))
 
-        file.write(songParser.lyrics)
-        file.close()
+        writeLyricsToFile(artistName, song[1], parser.lyrics)
+
+def writeLyricsToFile(artistName, songName, lyrics):
+    try:
+        file = open("lyrics-data/" + artistName + "/" + songName, "w")
+        print("Writing lyrics for " + songName + " to file")
+    except "FileNotFoundError":
+        print("Could not write lyrics for " + songName + " to file...")
+        break
+
+    file.write(lyrics)
+    file.close()
 
 
-response = requests.get('http://songmeanings.com/artist/directory/main/popular/') # top 50 artists
+artists = mineArtists('http://songmeanings.com/artist/directory/main/popular/') # top 50 artists
 
-# parser = ArtistSongParser()
-# print("Finding songs for artist...")
-# parser.feed(str(response.content))
-# print("Number of songs found: " + str(len(parser.songs)))
+for artist in artists:
+    # create artist directory
+    directory = "lyrics-data/" + artist[1]
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-parser = lyricParsers.ArtistParser()
-print("Finding artists...")
-parser.feed(str(response.content))
-print("Number of artists found: " + str(len(parser.artists)))
-print([x[1] for x in parser.artists])
-
-# import nltk
-
-# with open('lyrics-data/pink-floyd/A Great Day for Freedom.txt', 'r') as myfile:
-#     data=myfile.read()
-
-# words = nltk.word_tokenize(data)
-# print(words)
+# mine and write lyrics to file
+mineSongs(artists)
